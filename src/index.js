943cdacf9a173e6984cbbd164b8943a5584972e7
Amanda <62287246+amandasss@users.noreply.github.com>
2022-09-18 12:06:12 +0200

    Restructured index.js and sent props to Square

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
+class Game extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
-        </button>
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
       );
     }
   }
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />;
     }
   
     render() {
@@ -42,19 +48,13 @@ class Square extends React.Component {
       );
     }
   }
-  
-  class Game extends React.Component {
+
+  class Square extends React.Component {
     render() {
       return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
+        <button className="square">
+          {this.props.value}
+        </button>
       );
     }
   }
